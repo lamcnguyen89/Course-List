2ce6b5257fca3da66c5dd31a185feea0d3f4b8da
Lam Nguyen <lamcnguyen89@gmail.com>
2022-01-10 01:59:31 -0500

    Initial Commit

---
+++ Course List/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Course List
+//
+//  Created by Lam Nguyen on 1/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Course List/Course_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Course_ListApp.swift
+//  Course List
+//
+//  Created by Lam Nguyen on 1/10/22.
+//
+
+import SwiftUI
+
+@main
+struct Course_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
