f5df17aa0a4ed2d569dd35898e71c4ea68259ce4
Lam Nguyen <lamcnguyen89@gmail.com>
2022-01-10 02:17:59 -0500

    Added initial commit of tapping into an API

--- Course List/ContentView.swift
+++ Course List/ContentView.swift
@@ -10,7 +10,33 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         Text("Hello, world!")
-            .padding()
+            .onAppear(perform: {
+                getCourses()
+            })
+    }
+    
+    func getCourses() {
+        let apiString =  "https://zappycode.com/api/courses?format=json"
+        
+        if let apiURL = URL(string: apiString) {
+            var request = URLRequest(url: apiURL)
+            request.httpMethod = "GET"
+            
+            URLSession.shared.dataTask(with: request) {
+                (data, response, error) in
+                
+                if error != nil {
+                    print("There was an error")
+                } else if data != nil {
+                    let apiDataString = String(data: data!, encoding: .utf8)
+                    print(apiDataString ?? "Error")
+                }
+                
+                
+            }.resume()
+        }
+        
+        
     }
 }
 
